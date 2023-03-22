5110c11e226459d32f4d726e12d6be6316538dc2
heitor <heitorbrandolin@hotmail.com>
2023-03-22 11:55:56 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -7,4 +7,14 @@ public class Utils {
 	public static Stage currentStage(javafx.event.ActionEvent event) {
 		return (Stage) ((Node) event.getSource()) .getScene().getWindow();
 	}
+	
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e) {
+			return null;
+		}
+		
+	}
 }
